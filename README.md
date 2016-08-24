b1e7e817169c00f1d53680b0b19d30059385b663
Derek <dclifford@alianza.com>
2016-08-24 16:13:53 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# step-maven
+
+A simple maven executor written in `bash`
+
+# Options
+
+- `goals` String with all of the options you want to use
+- `skip_on_branch` Skips running this step if the branch specified is the one being run
+- `only_on_branch` Skips running this step if the branch specified is not the one being run
+
+
+# Example
+
+```yaml
+build:
+    steps:
+      - alianza/maven:
+        goals: -Dmaven.test.failure.ignore=false test
+        settings: $HOME/.m2/settings.xml
+        skip_on_branch: master
+
+      - alianza/maven:
+        goals: -Dmaven.test.failure.ignore=false deploy
+        settings: $HOME/.m2/settings.xml
+        only_on_branch: master
+```
+
+# License
+
+Apache 2.0
+
+# Changelog
+
+## 1.0.0
+
+- Initial release
---
+++ run.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# Some of this is modified to fit other needs from: https://bitbucket.org/xenoterracide/wercker-step-maven
+# Original Author of the above repo: Caleb Cushing
+
+set -e
+
+# if no settings are specified, default to the home default
+WERCKER_MAVEN_SETTINGS=${WERCKER_MAVEN_SETTINGS:-${HOME}/.m2/settings.xml}
+
+# check if maven is installed
+if ! type mvn &>/dev/null; then
+  error "mvn is not installed"
+else
+  debug "$(mvn -version)"
+fi
+
+# verify goals exist
+if [ -z "$WERCKER_MAVEN_GOALS" ]; then
+  fail "Must provide goals to run"
+fi
+
+# skip if we're on specified skipped branch
+if [ ! -z $WERCKER_MAVEN_SKIP_ON_BRANCH ]; then
+  for skip_if_branch in $WERCKER_MAVEN_SKIP_ON_BRANCH
+    do
+    if [ $WERCKER_GIT_BRANCH == $skip_if_branch ]; then
+      info "Skipping step due to being on branch ${WERCKER_GIT_BRANCH}"
+      exit 0
+    fi
+  done
+fi
+
+# skip if we're expecting to be on a branch we're not
+if [ ! -z $WERCKER_MAVEN_ONLY_ON_BRANCH ]; then
+  found=0
+  for only_on_branch in $WERCKER_MAVEN_SKIP_ON_BRANCH
+    do
+    if [ $WERCKER_GIT_BRANCH == $only_on_branch ]; then
+      found=1
+    fi
+  done
+  if [ $found -eq 0 ]; then
+    info "Skipping due to being on branch ${WERCKER_GIT_BRANCH} (expecting ${WERCKER_MAVEN_ONLY_ON_BRANCH})"
+  fi
+fi
+
+mvn --update-snapshots \
+    --batch-mode \
+    -Dmaven.repo.local=${WERCKER_CACHE_DIR} \
+    -s ${WERCKER_MAVEN_SETTINGS} \
+    ${WERCKER_MAVEN_GOALS}
+
+exitCode=$?
+
+exit exitCode;
+
