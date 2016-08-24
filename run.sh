8ee120c3a9fa01b898b056cb29c651a1941c2a55
Derek <dclifford@alianza.com>
2016-08-24 17:36:17 -0600

    add returns so skippers work

--- run.sh
+++ run.sh
@@ -18,34 +18,40 @@ if [ -z "$WERCKER_MAVEN_GOALS" ]; then
   fail "Must provide goals to run"
 fi
 
-# skip if we're on specified skipped branch
-if [ ! -z "$WERCKER_MAVEN_SKIP_ON_BRANCH" ]; then
-  for skip_if_branch in $WERCKER_MAVEN_SKIP_ON_BRANCH
-    do
-    if [ "$WERCKER_GIT_BRANCH" == "$skip_if_branch" ]; then
-      info "Skipping step due to being on branch ${WERCKER_GIT_BRANCH}"
-      exit 0
+function run {
+    # skip if we're on specified skipped branch
+    if [ ! -z "$WERCKER_MAVEN_SKIP_ON_BRANCH" ]; then
+      for skip_if_branch in $WERCKER_MAVEN_SKIP_ON_BRANCH
+        do
+        if [ "$WERCKER_GIT_BRANCH" == "$skip_if_branch" ]; then
+          info "Skipping step due to being on branch ${WERCKER_GIT_BRANCH}"
+          return 0
+        fi
+      done
     fi
-  done
-fi
 
-# skip if we're expecting to be on a branch we're not
-if [ ! -z "$WERCKER_MAVEN_ONLY_ON_BRANCH" ]; then
-  found=0
-  for only_on_branch in $WERCKER_MAVEN_SKIP_ON_BRANCH
-    do
-    if [ "$WERCKER_GIT_BRANCH" == "$only_on_branch" ]; then
-      found=1
+    # skip if we're expecting to be on a branch we're not
+    if [ ! -z "$WERCKER_MAVEN_ONLY_ON_BRANCH" ]; then
+      found=0
+      for only_on_branch in $WERCKER_MAVEN_SKIP_ON_BRANCH
+        do
+        if [ "$WERCKER_GIT_BRANCH" == "$only_on_branch" ]; then
+          found=1
+        fi
+      done
+      if [ $found -eq 0 ]; then
+        info "Skipping due to being on branch ${WERCKER_GIT_BRANCH} (expecting ${WERCKER_MAVEN_ONLY_ON_BRANCH})"
+        return 0
+      fi
     fi
-  done
-  if [ $found -eq 0 ]; then
-    info "Skipping due to being on branch ${WERCKER_GIT_BRANCH} (expecting ${WERCKER_MAVEN_ONLY_ON_BRANCH})"
-  fi
-fi
 
-mvn --update-snapshots \
-    --batch-mode \
-    -Dmaven.repo.local=${WERCKER_CACHE_DIR} \
-    -s ${WERCKER_MAVEN_SETTINGS} \
-    ${WERCKER_MAVEN_GOALS}
+    mvn --update-snapshots \
+        --batch-mode \
+        -Dmaven.repo.local=${WERCKER_CACHE_DIR} \
+        -s ${WERCKER_MAVEN_SETTINGS} \
+        ${WERCKER_MAVEN_GOALS}
 
+    return 0
+}
+
+run()
